5c2d1f2f475863237438d5e60140c44729388611
pranavK911 <pranavk82170@gmail.com>
2025-01-03 13:06:54 +0530

    second commit

---
+++ 3popcorn/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+// import Star from './Star';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    {/* <Star maxstar={10}/>
+    <Star/> */}
+  </React.StrictMode>
+);
+
+
